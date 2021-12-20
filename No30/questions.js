7d2c03e1b3af8779ebafbac1edf90472d2b3ae0e
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2021-12-21 04:55:31 +0900

    整理

---
+++ No30/questions.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("やっちゃえ！");
+
+// 変数の練習
+let hello = "Hello JavaScript!";
+console.log(hello);
+alert("一時停止");
+hello = "Hello php";
+console.log(hello);
---
+++ No31/questions.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("やっちゃえ！");
+
+// 変数の練習
+let hello = "Hello JavaScript!";
+console.log(hello);
+alert("一時停止");
+hello = "Hello php";
+console.log(hello);
+
+// 配列
+const hairetu = ["a","b","c","d"];
+console.table(hairetu);
+
+hairetu.forEach(hai => console.log(hai));
+
